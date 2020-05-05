f8bd2385f127b12e75e116404d6f97a6a576a925
Robert Paul <robertfpaul@gmail.com>
2020-05-05 10:44:38 -0500

    Added fully defined RandomForests parameters and improved the structure of the stats output file.

--- Quantify_ponding_distributions/Classification.R
+++ Quantify_ponding_distributions/Classification.R
@@ -10,12 +10,13 @@ library(RStoolbox)
 library(raster)
 
 # Check extents of '2017-05-10' and '2017-05-14'!!
-dateVec <-  c('2017-05-13')
+# dateVec <-  c('2017-08-19', '2017-09-09')
 # dateVec <-  c('2017-05-02', '2017-05-05', '2017-05-06', '2017-05-07',
-#               '2017-05-09', '2017-05-10', '2017-05-13', '2017-05-14',
-#               '2017-05-15')
+#                '2017-05-09', '2017-05-10', '2017-05-13', '2017-05-14',
+#                '2017-05-15')
+dateVec <- c('2017-05-02', '2017-05-09', '2017-05-13')
 
-userPath <- "~/tmp/2017-05/"
+userPath <- "~/Box_Sync/tmp/2017-05/"
 
 mosaicPath <- paste0(userPath, "Mosaics/")
 mosaicSuffix <- "_cbw_masked-mosaic_3m.tif"
@@ -23,7 +24,7 @@ mosaicSuffix <- "_cbw_masked-mosaic_3m.tif"
 naFlagPath <- paste0(userPath, "NA Flagged/")
 naFlagSuffix <- "_R_na-flagged_mosaic_3m.tif"
 
-trainerPath <- paste0(userPath, "2017-05_Combined_Trainers_Cai+Paul.shp")
+trainerPath <- "~/Box_Sync/GIS/Flooding_Remote_Sensing/PLabs/Combined_Trainer_Polys/2017-05_Combined_Trainers_Cai+Paul.shp"
 
 classPath <- paste0(userPath, "Classified/R_randomforest_bin-class_")
 classSuffix <- ".tif"
@@ -37,41 +38,52 @@ for(i in seq_len(length(dateVec))) {
   thisDate <- dateVec[i]
   print(paste0("Starting on ", thisDate))
   
-  filepath <- paste0(mosaicPath, thisDate, mosaicSuffix)
-  r <- brick(filepath)
-  
-  startTime <- Sys.time()
+  # # Open the mosaic that has zeroes where NAs should be
+  # filepath <- paste0(mosaicPath, thisDate, mosaicSuffix)
+  # r <- brick(filepath)
+  # 
+  # startTime <- Sys.time()
 
   filepath <- paste0(naFlagPath, thisDate, naFlagSuffix)
-  writeRaster(r,
-              filepath,
-              format = "GTiff",
-              overwrite = TRUE,
-              datatype = "INT2U",
-              NAflag = 0)
-  #endCluster()
-  endTime <- Sys.time()
-  print(paste0(filepath, " finished: ", endTime - startTime, " m"))
   
+  # Write the new raster with NAs flagged as 0 values
+  # writeRaster(r,
+  #             filepath,
+  #             format = "GTiff",
+  #             overwrite = TRUE,
+  #             datatype = "INT2U",
+  #             NAflag = 0)
+  # endTime <- Sys.time()
+  # print(paste0(filepath, " finished: ", endTime - startTime, " m"))
+  
+  # Load the NA-flagged files
   plabs <- brick(filepath)
   outPath <- paste0(classPath, thisDate, classSuffix)
   
   # Restrict to the date we're looking at
   train <- polysIn[polysIn$strDate == thisDate,]
+  # Redefine class factors; caret (via superClass) will use the first factor
+  # as the "positive" class
+  train$class <- factor(train$class, levels = c(1, 0))
   
   startTime <- Sys.time()
   # Begin CPU parallellization
   beginCluster()
-
-  ## Fit classifier (splitting training into 90% training data, 10% validation data)
+  
+  # Fit classifier (Random Forests; 50 trees; 10-fold cross-validation,
+  # splitting training into 90% training/CV data, 10% validation data; validate
+  # folds on Kappa value; cross-validate by polygon)
   # Should run parallelized
-  SC <- superClass(plabs, trainData = train, responseCol = "class", model = "rf",
-                   tuneLength = 1, nSamples = 500000,
+  SC <- superClass(plabs, trainData = train, responseCol = "class",
+                   model = "rf", nSamples = 275000, kfold = 10, minDist = 1, 
                    na.action = na.omit, trainPartition = 0.9, verbose = TRUE,
-                   predict = FALSE)
+                   predict = FALSE, metric = "Kappa", ntree = 50,
+                   polygonBasedCV = TRUE)
   endTime <- Sys.time()
   print(paste0("Finished training: ", endTime - startTime, " m"))
   
+  print(paste0("Starting prediction on ", thisDate))
+  startTime <- Sys.time()
   # Classification of predicted classification values, parallelized
   map <- clusterR(plabs,
                   predict,
@@ -88,9 +100,13 @@ for(i in seq_len(length(dateVec))) {
   endTime <- Sys.time()
   print(paste0(thisDate, " finished predicting: ", endTime - startTime, " m"))
   
-  # Report the training statistics
+  # Save the model and validation statistics to file
   sink(file = paste0(statsPath, thisDate, statsSuffix))
-  print(SC)
-  # Reset to output to console
+  print(thisDate)
+  print(SC$model)
+  print(SC$modelFit)
+  print(SC$validation$performance)
+  
+  # Reset to output to console only
   sink()
 }
